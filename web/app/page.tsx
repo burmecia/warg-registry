8f53fce123e01ff7dd4bd6bc3cc76ecb52200290
Bo Lu <lv.patrick@gmail.com>
2024-05-27 18:01:50 +1000

    update front page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,54 +1,22 @@
-import dayjs from "dayjs"
-import localizedFormat from "dayjs/plugin/localizedFormat"
-import utc from "dayjs/plugin/utc"
-import { Package } from "lucide-react"
+import { Boxes } from "lucide-react"
 import { Separator } from "@/components/ui/separator"
 import { getPackages } from "@/lib/db"
-import CopySpecButton from "@/components/copy-spec"
+import PkgList from "@/components/pkg-list"
 
 // Opt out of caching for all data requests in the route segment
 export const dynamic = 'force-dynamic'
-
-dayjs.extend(utc)
-dayjs.extend(localizedFormat)
-
-function makePkgSpec(rel: any) : string {
-  return "fdw_package_url 'wargs://wrappers.supabase.com',\n"
-    + `fdw_package_name '${rel.package}',\n`
-    + `fdw_package_version '${rel.version}',\n`
-}
 
 export default async function Home() {
   const pkgs = await getPackages()
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24 py-16">
+    <main className="flex min-h-screen flex-col items-center px-24 py-16">
       <section className="flex flex-row gap-4">
-        <Package size={40} />
-        <h1 className="text-4xl font-extrabold mb-8">Supabase Wrappers Registry</h1>
+        <Boxes size={40} className="text-emerald-500"/>
+        <h1 className="text-4xl font-extrabold mb-8">Supabase Wasm Wrappers Registry</h1>
       </section>
       <Separator />
-      <section>
-        <ol className="flex flex-col gap-3">
-        {pkgs.map((pkg: any) => (
-          <li key={pkg.log_id} className="">
-            <h2 className="text-2xl font-bold py-3">{pkg.name}</h2>
-            <ol className="flex flex-col gap-4 pl-4">
-            {pkg.releases.map((rel: any) => (
-              <li key={rel.record_id} className="flex flex-col gap-2 border-l-4 pl-2 border-emerald-500">
-                <div className="flex flex-row gap-2">
-                  <span className="flex items-center font-bold">v{rel.version}</span>
-                  <CopySpecButton spec={makePkgSpec(rel)} />
-                </div>
-                <div className="text-sm text-muted-foreground">{rel.status} at {dayjs.utc(rel.released_at).local().format("lll")}</div>
-                <div>content hash: {rel.content_hash}</div>
-              </li>
-            ))}
-            </ol>
-          </li>
-        ))}
-        </ol>
-      </section>
+      <PkgList pkgs={pkgs} />
       <section className="mt-8">
         &copy; Supabase Inc
       </section>
---
+++ web/components/pkg-list.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import dayjs from "dayjs"
+import localizedFormat from "dayjs/plugin/localizedFormat"
+import utc from "dayjs/plugin/utc"
+import { useState } from 'react'
+import { Package, Frown } from "lucide-react"
+import { Separator } from "@/components/ui/separator"
+import { Input } from "@/components/ui/input"
+import CopySpecButton from "@/components/copy-spec"
+
+dayjs.extend(utc)
+dayjs.extend(localizedFormat)
+
+function makePkgSpec(rel: any) : string {
+  return "fdw_package_url 'wargs://wrappers.supabase.com',\n"
+    + `fdw_package_name '${rel.package}',\n`
+    + `fdw_package_version '${rel.version}',\n`
+}
+
+export default function PkgList({ pkgs }: { pkgs: any[] }) {
+  const [filter, setFilter] = useState("")
+
+  return (
+    <section className="pt-4">
+      <Input placeholder="Search package" onChange={e => setFilter(e.target.value)} />
+      <ol className="flex flex-col gap-3">
+      {pkgs.filter((pkg: any) => pkg.name.includes(filter)).length === 0 &&
+        <li className="flex flex-row gap-2 py-6"><Frown /> No packages found</li>
+      }
+      {pkgs.filter((pkg: any) => pkg.name.includes(filter)).map((pkg: any) => (
+        <li key={pkg.log_id} className="">
+          <h2 className="flex flex-row gap-2 text-2xl font-bold py-3">
+            <div className="flex flex-col justify-center pt-0.5">
+              <Package size={24} className="text-emerald-500" />
+            </div>
+            {pkg.name}
+          </h2>
+          <ol className="flex flex-col gap-4 pl-5">
+          {pkg.releases.map((rel: any) => (
+            <li key={rel.record_id} className="flex flex-col gap-2 border-l-4 pl-2 border-emerald-500">
+              <div className="flex flex-row gap-2">
+                <span className="flex items-center font-bold">v{rel.version}</span>
+                <CopySpecButton spec={makePkgSpec(rel)} />
+              </div>
+              <div className="text-sm text-muted-foreground">{rel.status} at {dayjs.utc(rel.released_at).local().format("lll")}</div>
+              <div>content hash: {rel.content_hash}</div>
+            </li>
+          ))}
+          </ol>
+        </li>
+      ))}
+      </ol>
+    </section>
+  );
+}
